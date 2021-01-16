98c021d5f353fbb69de18ed0cfacca3f33956298
lucky <takuto.yoshikai@gmail.com>
2021-01-16 19:36:01 +0900

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const ipfsAPI = require("ipfs-api");
+const ipfs = ipfsAPI("ipfs.infura.io", "5001", { protocol: "https" });
+const fs = require("fs");
+const { BufferList } = require("bl");
+
+async function add(filePath) {
+  const data = fs.readFileSync(filePath);
+  const response = await ipfs.add(data);
+  if (response && response.length > 0) {
+    return response[0].path;
+  }
+  return null;
+}
+
+async function get(cid) {
+  for await (const file of ipfs.get(cid)) {
+    const content = new BufferList();
+    for await (const chunk of file.content) {
+      content.append(chunk);
+    }
+    content.pipe(fs.createWriteStream("./output.dat"));
+  }
+}
+module.exports = {
+  add,
+  get,
+}
